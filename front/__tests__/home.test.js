edfb93614a8f3e50f2fd78b7d7c28582fa443cd3
Isaac Maamouche <maamouche.isaac@gmail.com>
2022-10-29 11:37:27 +0200

    refactor: ready for presentation

--- front/__tests__/home.test.js
+++
@@ -1,8 +0,0 @@
-const { mockProductsData } = require("../js/utils/tests/mockTestData.mjs");
-
-describe("API - all products", () => {
-  const getProductsFromAPI = mockProductsData;
-  test("renders the 8 products on the home page", () => {
-    expect(getProductsFromAPI.length).toBe(8);
-  });
-});
